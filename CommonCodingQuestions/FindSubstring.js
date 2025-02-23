8928108be4e83a457d12084de9cea1562726b287
Victoria Truong <victoriataitruong@gmail.com>
2025-02-22 20:39:29 -0800

    removed files

--- CommonCodingQuestions/FindSubstring.js
+++
@@ -1,8 +0,0 @@
-//Write a program to find a substring
-
-function findSubstring(mainString, subString) {
-    return mainString.includes(subString);
-}
-
-console.log(findSubstring("Hello, world!", "world")); // true
-console.log(findSubstring("Hello, world!", "JavaScript")); // false
